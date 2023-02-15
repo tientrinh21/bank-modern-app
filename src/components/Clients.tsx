11db35d94e956afb9d64cc9940b547118381c97c
Tien Trinh <tqtien.a11619@gmail.com>
2023-02-15 15:48:39 +0900

    Add Testimonials and Clients

--- src/components/Clients.tsx
+++ src/components/Clients.tsx
@@ -1,7 +1,15 @@
-const Clients = () => {
-	return (
-		<div>Clients</div>
-	)
-}
+import { clients } from '../constants'
+
+const Clients = () => (
+  <section className="flex justify-center items-center my-4">
+    <div className="flex justify-center items-center flex-wrap sm:gap-10 gap-3 w-full">
+      {clients.map((client) => (
+        <div key={client.id} className="flex-1 flex justify-center items-center sm:min-w-[192px] min-w-[120px]">
+          <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain" />
+        </div>
+      ))}
+    </div>
+  </section>
+)
 
 export default Clients
--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -3,19 +3,20 @@ import FeedbackCard from './FeedbackCard'
 
 const Testimonials = () => (
   <section id="clients" className="flex justify-center items-center flex-col sm:py-16 py-6 relative">
-    {/* TODO: gradient */}
-    <div />
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
 
     <div className="w-full flex justify-center items-center md:flex-row flex-col sm:mb-16 mb:6 z-[1]">
       <h1 className="font-poppins font-semibold text-white xs:text-[48px] text-[40px] xs:leading-[76.8px] leading-[66.8px] w-full">
         What people are <br className="sm:block hidden" /> saying about us
       </h1>
       <div className="w-full md:mt-0 mt-6">
-        <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[450px]">Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+        <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[450px]">
+          Everything you need to accept card payments and grow your business anywhere on the planet.
+        </p>
       </div>
     </div>
 
-    <div className="flex flex-wrap sm:justify-start justify-center w-full relative z-[1] ">
+    <div className="flex flex-wrap sm:justify-start justify-center w-full relative z-[1]">
       {feedback.map((card) => (
         <FeedbackCard key={card.id} {...card} />
       ))}
